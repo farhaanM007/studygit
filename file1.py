bf752b06ce954ba32822f6f623f7be5e5f0a9b01
farhaanM007 <farhaan.mohammed@mozanta.com>
2023-11-06 12:41:02 +0530

    new changes

--- file1.py
+++ file1.py
@@ -1,9 +1,10 @@
-def add(a,b):
-    return a+b
+def add(a,b,d):
+    return a+b+d
 
 a=int(input("Enter a"))
 b=int(input("Enter b"))
+d=int(input("Enter d"))
 
-c=add(a,b)
+c=add(a,b,d)
 
 print(c)
