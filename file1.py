944b8228eba8d82ff1cbb507832694ebf60afc97
farhaanM007 <farhaan.mohammed@mozanta.com>
2023-11-06 10:39:57 +0530

    study

---
+++ file1.py
@@ -0,0 +1,9 @@
+def add(a,b):
+    return a+b
+
+a=int(input("Enter a"))
+b=int(input("Enter b"))
+
+c=add(a,b)
+
+print(c)
---
+++ file2.py
@@ -0,0 +1,9 @@
+def sub(a,b):
+    return a-b
+
+a=int(input("Enter a"))
+b=int(input("Enter b"))
+
+c=sub(a,b)
+
+print(c)
